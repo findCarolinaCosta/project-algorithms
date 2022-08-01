3f29e67f018830ef9af59a50b6be24d9759ae16a
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-08-01 20:22:43 -0300

    Fix: lint

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,7 +1,7 @@
 def study_schedule(permanence_period, target_time):
     counter = 0
     try:
-        for  input, output in permanence_period:
+        for input, output in permanence_period:
             if input <= target_time <= output:
                 counter += 1
 
