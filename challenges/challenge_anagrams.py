a35316019bb7bc532acef33dc646df08a8cb8543
findcarolinacosta <carolinadacosta1997@gmail.com>
2022-08-01 20:18:21 -0300

    Sort algorithm - is_anagram function

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,2 +1,15 @@
 def is_anagram(first_string, second_string):
-    """ Faça o código aqui. """
+    first = first_string.lower()
+    second = second_string.lower()
+
+    if (len(first) != len(second)):
+        return False
+
+    for character in first:
+        if character not in second:
+            return False
+
+        if first.count(character) != second.count(character):
+            return False
+
+    return True
